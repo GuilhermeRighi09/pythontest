2d709086aa5d078f9c9d90b3d94a018fe1b798c3
Guilherme Righi <150302062+GuilhermeRighi09@users.noreply.github.com>
2026-03-06 10:07:11 -0300

    Add files via upload

--- main.py
+++ main.py
@@ -6,6 +6,10 @@ app = Flask(__name__)
 def index():
     return "<h1>Hello World!!</h1>"
 
+@app.route("/curso")
+def curso():
+    return "<h1>Análise de Desenvolvimentos de Sistemas</h1>"
+
 if __name__ == "__main__":
     app.run()
 
